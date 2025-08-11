bc0870cae24e3f9413c27d199dcd7750cf60d863
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-11 16:45:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const message = document.getElementById("message");
+    message.textContent = "Thanks for visiting my portfolio!";
+    message.style.color = "#007BFF";
+}
