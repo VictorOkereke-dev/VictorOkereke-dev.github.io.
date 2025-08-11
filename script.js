7dc26718f6f7640bd103eb3295f45094c7c4ef40
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-12 00:47:34 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showMessage() {
-    const message = document.getElementById("message");
-    message.textContent = "Thanks for visiting my portfolio!";
-    message.style.color = "#007BFF";
-}
